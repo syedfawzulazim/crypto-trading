d1c6ac6b7c617a3ff4dc67b2842fe4ce0b05e2f3
Syed Fawzul Azim <adnansyed27@gmail.com>
2022-04-17 18:58:51 +0200

    custom colors added

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
+  darkMode: "class",
   content: ["./src/pages/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-bg": "#17171a",
+      },
+      backgroundColor: {
+        dark: "#17171a",
+      },
+    },
   },
   plugins: [],
 };

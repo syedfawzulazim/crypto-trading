5196c13789d3a148775de355fbeddb505a4194f1
Syed Fawzul Azim <adnansyed27@gmail.com>
2022-04-20 05:24:08 +0200

    real-time data fetched

--- client/src/components/Table/Index.tsx
+++ client/src/components/Table/Index.tsx
@@ -1,9 +1,52 @@
+import Image from "next/image";
 import React from "react";
 import { AiOutlineStar } from "react-icons/ai";
 import { BsCurrencyBitcoin } from "react-icons/bs";
+import { IData } from "../../types";
 import Pagination from "./Pagination";
 
-const Index: React.FC = () => {
+interface Props {
+  data: IData[];
+}
+
+const Index: React.FC<Props> = (props: Props) => {
+  const { data } = props;
+
+  const modifiedData: IData[] = [];
+
+  data.map((coin) => {
+    modifiedData.push({
+      id: coin.id,
+      symbol: coin.symbol.toUpperCase(),
+      name: coin.name,
+      image: coin.image,
+      current_price: coin.current_price,
+      market_cap: coin.market_cap,
+      market_cap_rank: coin.market_cap_rank,
+      fully_diluted_valuation: coin.fully_diluted_valuation,
+      total_volume: coin.total_volume,
+      high_24h: coin.high_24h,
+      low_24h: coin.low_24h,
+      price_change_24h: coin.price_change_24h,
+      price_change_percentage_24h: parseFloat(
+        coin.price_change_percentage_24h.toFixed(2)
+      ),
+      market_cap_change_24h: coin.market_cap_change_24h,
+      market_cap_change_percentage_24h: coin.market_cap_change_percentage_24h,
+      circulating_supply: coin.circulating_supply,
+      total_supply: coin.total_supply,
+      max_supply: coin.max_supply,
+      ath: coin.ath,
+      ath_change_percentage: coin.ath_change_percentage,
+      ath_date: coin.ath_date,
+      atl: coin.atl,
+      atl_change_percentage: coin.atl_change_percentage,
+      atl_date: coin.atl_date,
+      roi: coin.roi,
+      last_updated: coin.last_updated,
+    });
+  });
+
   return (
     <div className="pt-1 bg-slate-100 dark:bg-[#1f222c] text-slate-900 dark:text-white">
       <div className="xl:max-w-[1400px] mx-auto  pr-4 lg:pr-0 ">
@@ -11,103 +54,60 @@ const Index: React.FC = () => {
           <table className="w-full table-auto ">
             <thead>
               <tr className="text-xs border-b dark:border-slate-800 border-slate-400">
-                <th className="sticky left-0 dark:bg-black bg-slate-300 ">
+                <th className="sticky left-0 dark:bg-slate-800 bg-slate-100 ">
                   Name
                 </th>
                 <th className="py-2 pl-6 text-right ">Price</th>
                 <th className="py-2 pl-6 text-right">24h %</th>
-                <th className="py-2 pl-6 text-right">7d %</th>
                 <th className="py-2 pl-6 text-right">Market Cap</th>
                 <th className="py-2 pl-6 text-right">Volume(24h)</th>
                 <th className="py-2 pl-6 text-right">Circulating Supply</th>
-                <th className="px-6 py-2 text-right">Last 7 Days</th>
+                <th className="py-2 pl-6 text-right">ATH</th>
+                <th className="px-6 py-2 text-right">ATL</th>
               </tr>
             </thead>
             <tbody className="font-semibold ">
-              <tr className="border-b dark:border-slate-800 hover:bg-slate-300 dark:hover:bg-slate-800">
-                <td className="sticky left-0 px-8 py-1 dark:bg-black bg-slate-200">
-                  <div className="flex items-center">
-                    <span>
-                      <AiOutlineStar />
-                    </span>
-                    <a href="">
-                      <div className="flex items-center">
-                        <BsCurrencyBitcoin />
-                        <div className="px-2">
-                          <p>Bitcoin</p>
-                          <div className="flex ml-2 space-x-2 text-xs">
-                            <p>1</p>
-                            <p>BTC</p>
+              {modifiedData.map((coin) => (
+                <tr
+                  className="border-b dark:border-slate-800 hover:bg-slate-200 dark:hover:bg-slate-800"
+                  key={coin.id}
+                >
+                  <td className="sticky left-0 px-2 py-2  dark:bg-[#1f222c]  bg-slate-100 ">
+                    <div className="flex items-center space-x-4">
+                      <div className="flex flex-col justify-center cursor-pointer">
+                        <AiOutlineStar />
+                      </div>
+                      <a href="">
+                        <div className="flex items-center space-x-2">
+                          <Image src={coin.image} width={30} height={30} />
+                          <div className="px-2 lg-m-w:hidden">
+                            <p>{coin.name}</p>
+                            <div className="flex ml-2 space-x-2 text-xs">
+                              <p>{coin.market_cap_rank}</p>
+                              <p>{coin.symbol}</p>
+                            </div>
+                          </div>
+                          <div className="px-2 lg:hidden">
+                            <div className="flex ml-2 space-x-2 text-xs">
+                              <p>{coin.market_cap_rank}</p>
+                              <p>{coin.symbol}</p>
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    </a>
-                  </div>
-                </td>
-                <td className="pl-8 text-right">$42,551.34</td>
-                <td className="pl-8 text-right">0.19%</td>
-                <td className="pl-8 text-right">7.80%</td>
-                <td className="pl-8 text-right">$808,286,091,224</td>
-                <td className="pl-8 text-right">$14,696,942,052</td>
-                <td className="pl-8 text-right">19,007,806 BTC</td>
-                <td className="px-6 text-right">7 days Graph</td>
-              </tr>
-              <tr className="border-b dark:border-slate-800">
-                <td className="sticky left-0 px-8 py-1 dark:bg-black bg-slate-200">
-                  <div className="flex items-center">
-                    <span>
-                      <AiOutlineStar />
-                    </span>
-                    <a href="">
-                      <div className="flex items-center">
-                        <BsCurrencyBitcoin />
-                        <div className="px-2">
-                          <p>Bitcoin</p>
-                          <div className="flex ml-2 space-x-2 text-xs">
-                            <p>1</p>
-                            <p>BTC</p>
-                          </div>
-                        </div>
-                      </div>
-                    </a>
-                  </div>
-                </td>
-                <td className="text-right">$3,243.59</td>
-                <td className="text-right">1.03%</td>
-                <td className="text-right">6.82%</td>
-                <td className="text-right">$389,977,803,255</td>
-                <td className="text-right">$8,877,751,073</td>
-                <td className="text-right">120,319,490 ETH</td>
-                <td className="px-6 text-right">7 days Graph</td>
-              </tr>
-              <tr className="border-b dark:border-slate-800">
-                <td className="sticky left-0 px-8 py-1 dark:bg-black bg-slate-200">
-                  <div className="flex items-center">
-                    <span>
-                      <AiOutlineStar />
-                    </span>
-                    <a href="">
-                      <div className="flex items-center">
-                        <BsCurrencyBitcoin />
-                        <div className="px-2">
-                          <p>Bitcoin</p>
-                          <div className="flex ml-2 space-x-2 text-xs">
-                            <p>1</p>
-                            <p>BTC</p>
-                          </div>
-                        </div>
-                      </div>
-                    </a>
-                  </div>
-                </td>
-                <td className="text-right">$42,551.34</td>
-                <td className="text-right">0.19%</td>
-                <td className="text-right">7.80%</td>
-                <td className="text-right">$808,286,091,224</td>
-                <td className="text-right">$14,696,942,052</td>
-                <td className="text-right">19,007,806 BTC</td>
-                <td className="px-6 text-right">7 days Graph</td>
-              </tr>
+                      </a>
+                    </div>
+                  </td>
+                  <td className="pl-8 text-right">${coin.current_price}</td>
+                  <td className="pl-8 text-right">
+                    {coin.price_change_percentage_24h}%
+                  </td>
+                  <td className="pl-8 text-right">{coin.high_24h}</td>
+                  <td className="pl-8 text-right">{coin.market_cap}</td>
+                  <td className="pl-8 text-right">{coin.total_volume}</td>
+                  <td className="pl-8 text-right">{coin.circulating_supply}</td>
+                  <td className="px-6 text-right">7 days Graph</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,13 +1,31 @@
-import type { NextPage } from "next";
+import type { NextPage, InferGetStaticPropsType, GetStaticProps } from "next";
 import { Hero, Selection, Subscribe, Table } from "../components";
+import { IData } from "../types";
+import axios from "axios";
 
-const Home: NextPage = () => {
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await axios.get(
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd"
+  );
+
+  const data: IData[] = response.data;
+
+  return {
+    props: {
+      data,
+    },
+    revalidate: 60,
+  };
+};
+
+const Home = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
+  // console.log(props.data);
   return (
     <div className="text-white ">
       <div className="bg-gradient-to-b dark:from-[#222531] dark:to-[#000000] from-[#7190bd] to-[#f8fafd">
         <Hero />
         <Selection />
-        <Table />
+        <Table data={props.data} />
       </div>
       <Subscribe />
     </div>

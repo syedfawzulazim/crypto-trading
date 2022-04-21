83b08c7324096f7afd217cd0fc9e91c4742977e5
Syed Fawzul Azim <adnansyed27@gmail.com>
2022-04-21 18:28:47 +0200

    modified

--- client/src/context/index.ts
+++ client/src/context/index.ts
@@ -1,1 +1,2 @@
 export { default as SidebarContextProvider } from "./SidebarContext";
+export * from "./PaginationContext";
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -3,29 +3,29 @@ import { Hero, Selection, Subscribe, Table } from "../components";
 import { IData } from "../types";
 import axios from "axios";
 
-export const getStaticProps: GetStaticProps = async () => {
-  const response = await axios.get(
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd"
-  );
+// export const getStaticProps: GetStaticProps = async () => {
+//   const response = await axios.get(
+//     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false"
+//   );
 
-  const data: IData[] = response.data;
+//   const data: IData[] = response.data;
 
-  return {
-    props: {
-      data,
-    },
-    revalidate: 60,
-  };
-};
+//   return {
+//     props: {
+//       data,
+//     },
+//     revalidate: 60,
+//   };
+// };
 
-const Home = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
+const Home = () => {
   // console.log(props.data);
   return (
     <div className="text-white ">
       <div className="bg-gradient-to-b dark:from-[#222531] dark:to-[#000000] from-[#7190bd] to-[#f8fafd">
         <Hero />
         <Selection />
-        <Table data={props.data} />
+        <Table />
       </div>
       <Subscribe />
     </div>

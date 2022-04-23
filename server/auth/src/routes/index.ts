29601a10a6f915a10a80d009a56e7d95c34c9db9
Syed Fawzul Azim <adnansyed27@gmail.com>
2022-04-23 03:18:44 +0200

    routes added

---
+++ server/auth/src/routes/index.ts
@@ -0,0 +1,4 @@
+export { default as currentUserRouter } from "./current-user";
+export { default as signinRouter } from "./signin";
+export { default as signupRouter } from "./signup";
+export { default as signoutRouter } from "./signout";

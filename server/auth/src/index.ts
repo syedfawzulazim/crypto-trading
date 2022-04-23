89c10bd6cd5d3979045b14d577dca094e5305cf8
Syed Fawzul Azim <adnansyed27@gmail.com>
2022-04-23 03:19:35 +0200

    index file

--- server/auth/src/index.ts
+++ server/auth/src/index.ts
@@ -1,11 +1,30 @@
 import express from "express";
+import cors from "cors";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+
+import {
+  currentUserRouter,
+  signinRouter,
+  signupRouter,
+  signoutRouter,
+} from "./routes";
 
 const app = express();
-
 app.use(express.json());
+app.use(cors());
 
-let a: number = 7;
+app.use(currentUserRouter);
+app.use(signupRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+
+app.all("*", () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000");
+  console.log("Listening on port 3000!!!!!");
 });

73e9954a787cfc9759314ba3e8aaad3f8622358e
Syed Fawzul Azim <adnansyed27@gmail.com>
2022-04-27 19:26:23 +0200

    comment added

--- server/auth/src/middlewares/error-handler.ts
+++ server/auth/src/middlewares/error-handler.ts
@@ -11,6 +11,7 @@ export const errorHandler = (
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
+  //other wise
   res.status(400).send({
     errors: [
       {

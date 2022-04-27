ff8d135c69a10aa3b933e517953a1ca59de66b1c
Syed Fawzul Azim <adnansyed27@gmail.com>
2022-04-27 19:25:25 +0200

    cookiesSession added

--- server/auth/src/index.ts
+++ server/auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import cors from "cors";
+import cookieSession from "cookie-session";
 import mongoose from "mongoose";
 
 import { errorHandler } from "./middlewares/error-handler";
@@ -14,8 +15,16 @@ import {
 } from "./routes";
 
 const app = express();
+app.set("trust proxy", true);
+
 app.use(cors());
 app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signupRouter);
@@ -29,6 +38,9 @@ app.all("*", () => {
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT MUST BE DEFINED");
+  }
   try {
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
     console.log("DB");

1b8634ec4cbc16e6f8876389a4a15273f18da071
Syed Fawzul Azim <adnansyed27@gmail.com>
2022-04-27 19:25:51 +0200

    route modified

--- server/auth/src/routes/current-user.ts
+++ server/auth/src/routes/current-user.ts
@@ -1,9 +1,10 @@
 import express from "express";
-
+import jwt from "jsonwebtoken";
+import { currentUser } from "../middlewares/current-user";
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-  res.send("CurrentUser");
+router.get("/api/users/currentuser", currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export default router;

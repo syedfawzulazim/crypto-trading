05778dc455b639ec6729ae87f759c0aa11fd5baf
Syed Fawzul Azim <adnansyed27@gmail.com>
2022-05-03 15:45:36 +0200

    modified

--- server/auth/src/index.ts
+++ server/auth/src/index.ts
@@ -1,41 +1,5 @@
-import express from "express";
-import "express-async-errors";
-import cors from "cors";
-import cookieSession from "cookie-session";
 import mongoose from "mongoose";
-
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
-
-import {
-  currentUserRouter,
-  signinRouter,
-  signupRouter,
-  signoutRouter,
-} from "./routes";
-
-const app = express();
-app.set("trust proxy", true);
-
-app.use(cors());
-app.use(express.json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signupRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-
-app.all("*", () => {
-  throw new NotFoundError("Route Not Found");
-});
-
-app.use(errorHandler);
+import { app } from "./app";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {

4446dba4ece77b9447e176a785d752a78eb85923
Syed Fawzul Azim <adnansyed27@gmail.com>
2022-05-18 03:19:00 +0200

    animation added

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./src/pages/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}"],
   theme: {
     extend: {
+      animation: {
+        blink: "blink 1.2s infinite",
+      },
+      keyframes: {
+        blink: {
+          "0%, 100%": { opacity: "0.2" },
+          "20%": { opacity: "1" },
+        },
+      },
+
       colors: {
         "dark-bg": "#17171a",
       },
